e084ac451b4933695aa896d05cd49dab85c54ab2
Wasim Rahman <wasimr95@hotmail.com>
2023-06-13 23:31:20 +0100

    Few links need to be added and projects needs to be added but portfilo finalized

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Flow in effect in Skills section
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -9,7 +10,18 @@ const observer = new IntersectionObserver((entries) => {
 
     });
 });
-
-
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Typewriter effect
+var i = 0;
+var txt = 'Hello. My name is Wasim Rahman!'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".intro_text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
